1c99ddc811a06387b46f79154f1efa421e6a2eb6
Alexey <avdeev.hexer@yandex.ru>
2021-10-09 11:10:09 +0300

    3.2 Organization of navigation - video - finalized - keyboard hiding removed

--- app/src/main/java/ru/netology/nmedia/utils/AndroidUtils.kt
+++ app/src/main/java/ru/netology/nmedia/utils/AndroidUtils.kt
@@ -1,16 +1,8 @@
 package ru.netology.nmedia.utils
 
 import android.content.Context
-import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 
-object AndroidUtils {
-    fun hideKeyboard(view: View) {
-        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken,0)
-    }
-}
 fun Context.showMyMessage(text: Int, length: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(
         this,
