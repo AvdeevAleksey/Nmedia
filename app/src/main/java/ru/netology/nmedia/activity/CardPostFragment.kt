a3b09fd0b2e06840f18ea4256249692c02d95227
user <avdeev.hexer@yandex.ru>
2021-10-12 12:02:28 +0300

    3.4 Fragments, FragmentManager - finalized - cardPost displayed

--- app/src/main/java/ru/netology/nmedia/activity/CardPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/CardPostFragment.kt
@@ -6,13 +6,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.map
 import androidx.navigation.fragment.findNavController
 import ru.netology.nmedia.R
 import ru.netology.nmedia.adapter.OnInteractionListener
 import ru.netology.nmedia.adapter.PostsAdapter
+import ru.netology.nmedia.adapter.countMyClick
 import ru.netology.nmedia.databinding.FragmentCardPostBinding
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.utils.AndroidUtils
@@ -30,65 +34,89 @@ class CardPostFragment : Fragment() {
 
         val binding = FragmentCardPostBinding.inflate(inflater, container, false)
 
-        val post : Post? = arguments?.getParcelable<Post>(AndroidUtils.POST_KEY)
-        arguments?.remove(AndroidUtils.POST_KEY)
+        val postId = arguments?.getParcelable<Post>(AndroidUtils.POST_KEY)?.id
 
-        val postsAdapter = PostsAdapter (object : OnInteractionListener {
-            override fun onLike(post: Post) {
-                viewModel.likeById(post.id)
-            }
-            override fun onShare(post: Post) {
-                val intent = Intent().apply {
-                    action = Intent.ACTION_SEND
-                    putExtra(Intent.EXTRA_TEXT,post.content)
-                    type = "text/plane"
-                }
-                val shareIntent =
-                    Intent.createChooser(intent,getString(R.string.chooser_share_post))
-                startActivity(shareIntent)
-                viewModel.shareById(post.id)
-            }
-            override fun onViewing(post: Post) {
-                viewModel.viewingById(post.id)
-            }
-            override fun onPostEdit(post: Post) {
-                findNavController().navigate(
-                    R.id.action_feedFragment_to_postFragment,
-                    bundleOf("post" to post)
-                )
-                viewModel.editPost(post)
-            }
-            override fun onPostRemove(post: Post) {
-                viewModel.removeById(post.id)
-            }
-            override fun onPlayVideo(post: Post) {
-                val intent = Intent().apply {
-                    Intent.ACTION_VIEW
-                    Uri.parse(post.videoInPost).takeIf {
-                        it != null
-                    }
-                }
-                val playVideoIntent = Intent.createChooser(intent,getString(R.string.play_video_app_chooser))
-                startActivity(playVideoIntent)
-            }
-
-            override fun onPostOpen(post: Post) {
-            }
-        })
-
-//        binding.cardPost.???? = postsAdapter
-//        TODO("I don't know where to submit a postAdapter")
-
-        viewModel.data.observe(viewLifecycleOwner) { posts ->
-            postsAdapter.submitList(posts)
-        }
-
-        viewModel.edited.observe(viewLifecycleOwner) { post ->
-            if (post.id == 0) {
+        viewModel.data.map { posts ->
+            posts.find { it.id == postId }
+        }.observe(viewLifecycleOwner) { post ->
+            post ?: run {
+                findNavController().navigateUp()
                 return@observe
+            }
+
+            binding.apply {
+                authorTextView.text = post.author
+                publishedTextView.text = post.published
+                contentTextView.text = post.content
+                videoContent.setImageURI(Uri.parse(post.videoInPost))
+                shareImageButton.text = countMyClick(post.shareCount)
+                viewsImageButton.text = countMyClick(post.viewingCount)
+                likeImageButton.isChecked = post.likedByMe
+                likeImageButton.text = countMyClick(post.likesCount)
+                videoGroup.isVisible = post.videoInPost.isNotBlank()
+
+                playVideoButton.setOnClickListener {
+                    playVideo(post)
+                }
+                videoGroup.setOnClickListener {
+                    playVideo(post)
+                }
+                likeImageButton.setOnClickListener {
+                    viewModel.likeById(post.id)
+                }
+                shareImageButton.setOnClickListener {
+                    val intent = Intent().apply {
+                        action = Intent.ACTION_SEND
+                        putExtra(Intent.EXTRA_TEXT, post.content)
+                        type = "text/plane"
+                    }
+                    val shareIntent =
+                        Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                    startActivity(shareIntent)
+
+                    viewModel.shareById(post.id)
+                }
+                viewsImageButton.setOnClickListener {
+                    viewModel.viewingById(post.id)
+                }
+                postMenuImageView.setOnClickListener {
+                    PopupMenu(it.context, it).apply {
+                        inflate(R.menu.options_post_menu)
+                        setOnMenuItemClickListener { item ->
+                            when (item.itemId) {
+                                R.id.postRemove -> {
+                                    viewModel.removeById(post.id)
+                                    true
+                                }
+                                R.id.postEdit -> {
+                                    findNavController().navigate(
+                                        R.id.action_fragmentCardPost_to_postFragment,
+                                        bundleOf(AndroidUtils.POST_KEY to post)
+                                    )
+                                    viewModel.editPost(post)
+                                    true
+                                }
+                                else -> false
+                            }
+
+                        }
+                    }.show()
+                }
             }
         }
 
         return binding.root
+    }
+
+    private fun playVideo(post: Post) {
+        val intent = Intent().apply {
+            Intent.ACTION_VIEW
+            Uri.parse(post.videoInPost).takeIf {
+                it != null
+            }
+        }
+        val playVideoIntent =
+            Intent.createChooser(intent, getString(R.string.play_video_app_chooser))
+        startActivity(playVideoIntent)
     }
 }
